19f968092727f0ae287e60200e11134fd75b8aae
Zecony <zeczdenko@yahoo.com>
2022-01-19 20:59:32 +1300

    Initial Complete Version 1.0

---
+++ index.js
@@ -0,0 +1,95 @@
+
+
+/*document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick() {
+  alert("I got clicked!");
+}*/
+
+
+// DETECTING BUTTON PRESSED
+var drums = document.getElementsByClassName("drum");
+
+if (drums) {
+  for (var i=0; i < drums.length; i++) {
+    drums[i].addEventListener("click", function () {
+
+      var buttonInnerHTML = this.innerHTML;
+
+      makeSound(buttonInnerHTML);
+
+      buttonAnimation(buttonInnerHTML);
+
+    });
+  }
+}
+
+// DETECTING KEYBOARD PRESSED
+document.addEventListener("keydown", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+// detecting which sound to play
+function makeSound(key) {
+  switch (key) {
+    case "w":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+         tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+       tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+       tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+       tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+       snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+       crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+       kick.play();
+      break;
+
+    default: console.log(buttonInnerHTML);
+
+  }
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeKey = document.querySelector("." + currentKey);
+
+  activeKey.classList.add("pressed");
+
+  setTimeout(function(){
+    activeKey.classList.remove("pressed");
+  }, 100);
+}
+
+/*function handleClick() {
+  var audio = new Audio('sounds/tom-1.mp3');
+audio.play();
+}*/
+
+// this.style.color  = "white";
